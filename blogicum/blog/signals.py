d5341e67047258125b3b57ea93c30ad5b0b0b342
mefikan <123304791+mefikan@users.noreply.github.com>
2024-12-08 00:03:12 +0300

    Update signals.py

--- blogicum/blog/signals.py
+++ blogicum/blog/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Comment, Post
+from .models import Comment, Post  # noqa: F401
 
 
 @receiver(post_save, sender=Comment)
